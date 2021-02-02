b75d3f5a7a95d2d0693115cf1a9160a5aec640c0
John <myschoolacountsacount@gmail.com>
2021-02-02 13:46:47 -0800

    Added child window creation function in the main.js

--- main.js
+++ main.js
@@ -3,69 +3,88 @@ const {app, BrowserWindow} = require('electron')
 const debug = /--debug/.test(process.argv[2])
 
 let mainWindow = null
-
+var a;
 function initialize () {
   
-  makeSingleInstance()
+	makeSingleInstance()
 
-  function createWindow () {
-    const windowOptions = {
-      width: 800,
-      minWidth: 400,
-      height: 800,
-      title: app.getName(),
-      webPreferences: {
-        nodeIntegration: true
-      }
-    }
+	function createWindow () {
+		const windowOptions = {
+      		width: 800,
+      		minWidth: 400,
+      		height: 800,
+      		title: app.getName(),
+      			webPreferences: {
+        			nodeIntegration: true
+      			}
+    		}
 
-    if (process.platform === 'linux') {
-      windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
-    }
+    		if (process.platform === 'linux') {
+      		windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
+    		}
 
-    mainWindow = new BrowserWindow(windowOptions)
-    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+   	 	mainWindow = new BrowserWindow(windowOptions)
+    		//mainWindow.loadURL("http://www.google.com")
+		mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
 
-    // Launch fullscreen with DevTools open, usage: npm run debug
-    if (debug) {
-      mainWindow.webContents.openDevTools()
-      mainWindow.maximize()
-      require('devtron').install()
-    }
+    		// Launch fullscreen with DevTools open, usage: npm run debug
+    		if (debug) {
+      			mainWindow.webContents.openDevTools()
+     			mainWindow.maximize()
+      			require('devtron').install()
+    		}
 
-    mainWindow.on('closed', () => {
-      mainWindow = null
-    })
-  }
+    		mainWindow.on('closed', () => {
+      		mainWindow = null
+   		})
+	}
 
-  app.on('ready', () => {
-    createWindow()
-  })
+	app.on('ready', () => {
+		createWindow()
+	})
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
+	app.on('window-all-closed', () => {
+    		if (process.platform !== 'darwin') {
+      			app.quit()
+    		}
+	})
 
-  app.on('activate', () => {
-    if (mainWindow === null) {
-      createWindow()
-    }
-  })
+	app.on('activate', () => {
+    		if (mainWindow === null) {
+      			createWindow()
+    		}
+	})
+}
+
+function createAWindow(a){
+	const windowOptions = {
+                width: 400,
+                minWidth: 300,
+                height: 600,
+		webPreferences: {
+                	nodeIntegration: true
+                },
+		parent: mainWindow
+        }
+	
+	aWindow = new BrowserWindow(windowOptions)
+	aWindow.loadURL(a)
+	//awindow.loadURL("http://www.google.com")
+	createWindow()
+	aWindow.show()
 }
 
 function makeSingleInstance () {
-  if (process.mas) return
+  	if (process.mas) return
 
-  app.requestSingleInstanceLock()
+  	app.requestSingleInstanceLock()
 
-  app.on('second-instance', () => {
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore()
-      mainWindow.focus()
-    }
-  })
+  	app.on('second-instance', () => {
+    		if (mainWindow) {
+     			 if (mainWindow.isMinimized()) mainWindow.restore()
+      			 	mainWindow.focus()
+    		}
+  	})
 }
 
 initialize()
